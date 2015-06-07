ba1ec2a13a5dfe45b19158509fde7e59c60643c6
jiacai2050 <jiacai2050@gmail.com>
2015-06-07 14:29:40 +0800

    add cron.js

---
+++ cron.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').exec; 
+var path = require("path");
+var CronJob = require('cron').CronJob;
+new CronJob('0 10 7-23/12 * * *', function() {
+    console.log(new Date());
+    var script = path.join(__dirname, "cron.sh");
+    exec(script, function(err, stdout, stderr) {
+        if(err) console.log(err);
+        console.log(stdout);
+        console.log(stderr);
+    });
+}, null, true, "Asia/Shanghai");
